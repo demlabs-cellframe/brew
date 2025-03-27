ed33951f7939bde6a48325a3be16d7915a8127d5
GitLab CI Node project <ci@gitlab.com>
2025-03-27 07:40:16 +0300

    Update cellframe-node.rb to version 5.3-357 from GitLab CI

--- Casks/cellframe-node.rb
+++ Casks/cellframe-node.rb
@@ -1,6 +1,6 @@
 cask "cellframe-node" do
   version "5.3-357"
-  sha256 "a7869eb7ec1d1756ec5db6b12b8ff4fef1083479d9bafc9b5e98edb9938a19a2"
+  sha256 "69263d832fed3c0adb6cd5d153eaa56df1d95c8914424f166b9a82cd8d262c2a"
   url "https://internal-pub.cellframe.net/macos/cellframe-node/feature-7706//cellframe-node-5.3-357-amd64-signed.pkg"
   name "Cellframe node"
   desc "Cellframe node"
