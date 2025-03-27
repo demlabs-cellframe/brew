730d7690f5195cc812bafdd18b75b89d19051902
GitLab CI Wallet project <ci@gitlab.com>
2025-03-27 11:06:44 +0300

    Update cellframe-wallet.rb to version 4.0.3 from GitLab CI

--- Casks/cellframe-wallet.rb
+++ Casks/cellframe-wallet.rb
@@ -1,6 +1,6 @@
 cask "cellframe-wallet" do
   version "4.0.3"
-  sha256 "728f5a645d0d619274525125fdfd572ae77eae03b913e0484641c652ec4fc00a"
+  sha256 "55539ff5c7ddf905a132450f73144e31ed255e1bb7ad705cefc537e8f28702e4"
   url "https://internal-pub.cellframe.net/macos/cellframe-wallet/feature-7706/cellframe-wallet_4.0.3_amd64-signed.pkg"
   name "Cellframe Wallet"
   desc "Cellframe Wallet"
